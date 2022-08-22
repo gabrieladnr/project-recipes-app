dca5b68e2fcc646748b0c58c62dfd9a00d5920aa
Gabriela Delamare <gabrieladnr@gmail.com>
2022-08-22 20:01:27 -0300

    req 19 parcial

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -18,27 +18,27 @@ class Recipes extends React.Component {
   //   <img data-testid={`${meals.index}-card-img`}
   // type="image/svg+xml"
   // data={ meals.strMealThumb }>Recipe Img</img>
+
   // const carImgId = `${index}-card-img`;
+  // const cardId = `${index}-recipe-card`;
+  // const cardNameId = `${index}-card-name`;
 
   // renderiza as primeiras 12 receitas de comida da API
   renderMeals() {
     const { meals } = this.props;
-    // const cardId = `${index}-recipe-card`;
-    // const cardNameId = `${index}-card-name`;
-    return meals?.map((meal, index) => {
-    (
+    return meals.length && meals.map((meal, index) => (
       <div key={ meal.idMeal } data-testid={ `${index}-recipe-card` }>
         <p data-testid={ `${index}-card-name` }>{ meal.strMeal }</p>
       </div>
-    )});
+    ));
   }
 
   render() {
-    const { meals } = this.props;
+    // const { meals } = this.props;
     return (
       <div>
         <p>Food Recipes</p>
-        {meals?.map((meal) => <p key={ meal.strMeal }>{meal.strMeal}</p>)}
+        {/* {meals?.map((meal) => <p key={ meal.strMeal }>{meal.strMeal}</p>)} */}
         <div>{this.renderMeals()}</div>
       </div>
     );
