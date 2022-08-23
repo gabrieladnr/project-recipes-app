3f302af4aae45e0d197c533f1caba53fd45dd729
Breno Prado <breno.o.prado@hotmail.com>
2022-08-23 14:05:54 -0300

    arquivo para teste dos detalhes criado

---
+++ src/tests/RecipesDetail.test.js
@@ -0,0 +1,9 @@
+import '@testing-library/jest-dom';
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { renderWithRouterAndRedux } from './renderWith';
+
+describe('Testando o componente <FoodDetail />', () => {
+
+});
