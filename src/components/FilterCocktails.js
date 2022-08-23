92675d7da6dc3c55c97cd6cb10b72714b86580e4
Gabriela Delamare <gabrieladnr@gmail.com>
2022-08-23 12:32:18 -0300

    cmt p/ switch branch

--- src/components/FilterCocktails.js
+++ src/components/FilterCocktails.js
@@ -3,6 +3,13 @@ import propTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 class FilterCocktails extends React.Component {
+  // implementa o filtro por categoria ao clicar no botão de categoria,
+  // renderizando no click 12 receitas da categoria
+
+  // handleCategoryButtonClick() {
+
+  // }
+
   // renderiza 5 botões com as categorias de drinks
   renderFilterCocktailsCategoryButtons() {
     const { cocktailCategories } = this.props;
@@ -13,7 +20,7 @@ class FilterCocktails extends React.Component {
           type="button"
           key={ drink.strCategory }
           data-testid={ `${drink.strCategory}-category-filter` }
-        // onClick={}
+          // onClick={ this.handleCategoryButtonClick() }
         >
           { drink.strCategory}
         </button>));
