aa6de9ed2a71c0d6a03ab41b27dd060796dbcfe4
leonardo-schneider2 <leocoelho256@gmail.com>
2022-08-28 20:39:28 -0300

    l

---
+++ src/test/FoodDetails.test.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouterAndRedux from './helpers/renderWithRouterAndRedux';
+import App from '../App';
+import favoriteRecipes, { doneRecipes } from './helpers/favoritesLocalStorage';
+import oneMeal from '../../cypress/mocks/oneMeal';
+import drinks from '../../cypress/mocks/drinks';
+
+describe('Testando o componente Drink Details:', () => {
+  beforeEach(() => {
+    global.fetch = async (url) => {
+      console.log(url);
+      switch (url) {
+      case 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=52771':
+        return {
+          json: jest.fn().mockResolvedValue(oneMeal),
+        };
+      default:
+        return {
+          json: jest.fn().mockResolvedValue(drinks),
+        };
+      }
+    };
+  });
+
+  test('botão de Continue a receita:', async () => {
+    localStorage.getItem('doneRecipes', JSON.stringify(doneRecipes));
+    localStorage.setItem('inProgressRecipes', '{"meals":{"52771":{"id":"52771"}}}');
+    renderWithRouterAndRedux(<App />, '/foods/52771');
+    await waitFor(() => {
+      screen.getByTestId('4-recomendation-card');
+    });
+    userEvent.click(screen.getByTestId('start-recipe-btn'));
+    screen.logTestingPlaygroundURL();
+  });
+
+  test('botão de Continue a receita:', async () => {
+    console.log(JSON.parse(localStorage.getItem('favoriteRecipes')));
+    localStorage.setItem('doneRecipes', JSON.stringify(doneRecipes));
+    localStorage.setItem('inProgressRecipes', '{"meals":{"52771":{"id":"52771"}}}');
+    renderWithRouterAndRedux(<App />, '/foods/52771');
+    await waitFor(() => {
+      screen.getByTestId('4-recomendation-card');
+    });
+    screen.logTestingPlaygroundURL();
+  });
+});
