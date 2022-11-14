e4748c15de3a6fc3774ad87c9dbafa771d685a7f
Gabriela Delamare <94017557+gabrieladnr@users.noreply.github.com>
2022-11-14 17:26:11 -0300

    add new branch

---
+++ trybe-filter-repo.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+### GIT FILTER-REPO ###
+
+## NÃO EXECUTE ESSE SCRIPT DIRETAMENTE
+## Esse script foi feito para uso do
+## script 'trybe-publisher' fornecido
+## pela Trybe.
+
+[[ $# == 1 ]] && \
+[[ $1 == "trybe-security-parameter" ]] && \
+git filter-repo \
+    --path .trybe \
+    --path .github \
+    --path trybe.yml \
+    --path trybe-filter-repo.sh \
+    --path cypress \
+    --path pull-request-para-branch-do-grupo.png \
+    --path only-all-green.png \
+    --path describe-only.png \
+    --path cypress.json \
+    --path README.md \
+    --invert-paths --force --quiet
